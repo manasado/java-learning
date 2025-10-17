770a6c192cb82f080698e00185525aec8950348c
Manasa D O <manasa.d.o@intimetec.com>
2025-10-17 14:58:55 +0530

    practiesmanasa2

--- src/addition.java
+++ src/addition.java
@@ -1,8 +1,10 @@
 public class addition {
-    int a=10,b=11,sum, sub;
-    sum=a+b;
-    sub=a-b;
-    System.out.printoutln(sub);
-    System.out.printoutln(sum);
-    System.out.printoutln(sub);
+    public static void main(String[] arg) {
+        int a = 10, z = 11, sum, sub;
+        sum = a + z;
+        sub = a - z;
+        System.out.println(sub);
+        System.out.println(sum);
+        System.out.println(sub);
+    }
 }
