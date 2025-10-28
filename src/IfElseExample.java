90b405cf0c34137f97d8d9c393b9412b0a990b8d
Manasa D O <manasa.d.o@intimetec.com>
2025-10-28 13:05:35 +0530

    IfElse profram

---
+++ src/IfElseExample.java
@@ -0,0 +1,10 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+        int marks = 45;
+        if (marks >= 50) {   //Two possible paths
+            System.out.println("You passed the exam!");
+        } else {
+            System.out.println("You failed the exam!");
+        }
+    }
+}
