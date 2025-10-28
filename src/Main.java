283334621363fc419a419b93c8f3e27bbbb609f3
Manasa D O <manasa.d.o@intimetec.com>
2025-10-28 10:52:17 +0530

    Objectclass created

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int a= 10,b=20,c;
-        c=a+b;
-        System.out.println(c);
-    }
-}
