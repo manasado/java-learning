f4e28b4c0ce6e9d39b25640ab818a40ee5a4e4b7
Manasa D O <manasa.d.o@intimetec.com>
2025-10-28 12:49:57 +0530

    usingAccesModifierwith Data type

---
+++ src/usingAccesModiferwithDataType.java
@@ -0,0 +1,16 @@
+public class usingAccesModiferwithDataType {
+    public int age = 25;       // public variable, type = int
+    private String name = "Manasa"; // private variable, type = String
+
+    public void showDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        usingAccesModiferwithDataType D = new usingAccesModiferwithDataType();
+        D.showDetails(); // Accessing public method
+    }
+}
