2ad3a7a8b3ff242c563cd74ea0ac4c42d9d7c7fc
Manasa D O <manasa.d.o@intimetec.com>
2025-10-30 12:06:59 +0530

    if statement program

---
+++ src/ifpractices.java
@@ -0,0 +1,11 @@
+public class ifpractices {
+    public class m{
+        public static void main(String[] args){
+            int a=15;
+            int b=20;
+            if (a+b>30){
+                System.out.println("a+b is greater than 30");
+            }
+        }
+    }
+}
